af3ba0b2bf287251cd7dca5fe3d013657acd6a26
droid664 <67774183+droid664@users.noreply.github.com>
2023-07-11 10:37:48 +0500

    Add files via upload
    add .gitignore, add babel and update webpack.config

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
+function test() {
+	return 'Это текст'
+}
 
+class Circle {
+	constructor() {
+		this.radius = 50
+	}
+	draw() {
+		return this.radius
+	}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,118 +1,135 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin')
 const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 const path = require('path')
 
-module.exports = {
-    entry: path.join(__dirname, 'src/js/app.js'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index.[contenthash].js',
-        assetModuleFilename: 'assets/img/[name][ext]',
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src/pages', 'index.twig'),
-            filename: 'index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
-        // new ImageminWebpWebpackPlugin(),
-    ],
-    devServer: {
-        watchFiles: path.join(__dirname, 'src'),
-        port: 3000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.twig$/,
-                use: [
-                    'html-loader',
-                    {
-                        loader: 'twig-html-loader',
-                        options: {
-                            data: (context) => {
-                                const data = path.join(__dirname, 'data.json')
-                                context.addDependency(data) // Force webpack to watch file
-                                return context.fs.readJsonSync(data, { throws: false }) || {}
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.svg$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: path.join('icons', '[name].[contenthash][ext]'),
-                },
-            },
-            {
-                test: /\.(woff2?|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [
-            new ImageMinimizerPlugin({
-                minimizer: {
-                    implementation: ImageMinimizerPlugin.sharpMinify,
-                    options: {
-                        encodeOptions: {
-                            encodeOptions: {
-                                jpeg: {
-                                    // https://sharp.pixelplumbing.com/api-output#jpeg
-                                    quality: 100,
-                                },
-                                webp: {
-                                    // https://sharp.pixelplumbing.com/api-output#webp
-                                    lossless: true,
-                                },
-                                avif: {
-                                    // https://sharp.pixelplumbing.com/api-output#avif
-                                    lossless: true,
-                                },
+function generateHtmlPlugins(templateDir) {
+	const fs = require('fs')
+	const path = require('path')
 
-                                // png by default sets the quality to 100%, which is same as lossless
-                                // https://sharp.pixelplumbing.com/api-output#png
-                                png: {},
+	// Get all template files from the specified directory
+	const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
 
-                                // gif does not support lossless compression at all
-                                // https://sharp.pixelplumbing.com/api-output#gif
-                                gif: {},
-                            },
-                        },
-                    },
-                },
-                generator: [
-                    {
-                        // You can apply generator using `?as=webp`, you can use any name and provide more options
-                        preset: 'webp',
-                        implementation: ImageMinimizerPlugin.imageminGenerate,
-                        options: {
-                            plugins: ['imagemin-webp'],
-                        },
-                    },
-                ],
-            }),
-        ],
-    },
+	return templateFiles.map((file) => {
+		// Remove file extension from the template filename
+		const name = file.replace('.twig', '')
+
+		return new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, templateDir, file),
+			filename: `${name}.html`,
+		})
+	})
+}
+
+module.exports = (env) => {
+	const isProduction = env.mode === 'production'
+
+	return {
+		entry: path.join(__dirname, 'src/js/app.js'),
+		output: {
+			path: path.join(__dirname, 'dist'),
+			filename: 'index.[contenthash].js',
+			assetModuleFilename: 'assets/[name][ext]',
+			clean: isProduction ? true : false,
+		},
+		plugins: [
+			...generateHtmlPlugins('src/pages'),
+			new MiniCssExtractPlugin({
+				filename: '[name].[contenthash].css',
+			}),
+		],
+		devServer: {
+			watchFiles: path.resolve(__dirname, 'src'),
+			port: 3000,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.twig$/,
+					use: [
+						'html-loader',
+						{
+							loader: 'twig-html-loader',
+							options: {
+								data: (context) => {
+									const data = path.join(__dirname, 'data.json')
+									context.addDependency(data) // Force webpack to watch file
+									return context.fs.readJsonSync(data, { throws: false }) || {}
+								},
+							},
+						},
+					],
+				},
+				{
+					test: /\.js$/,
+					use: ['babel-loader'],
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.(scss|css)$/,
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+				},
+				{
+					test: /\.(png|jpg|jpeg|gif)$/i,
+					type: 'asset/resource',
+				},
+				{
+					test: /\.svg$/,
+					type: 'asset/resource',
+					generator: {
+						filename: path.join('icons', '[name].[contenthash][ext]'),
+					},
+				},
+				{
+					test: /\.(woff2?|eot|ttf|otf)$/i,
+					type: 'asset/resource',
+				},
+			],
+		},
+		optimization: {
+			minimizer: [
+				new ImageMinimizerPlugin({
+					minimizer: {
+						implementation: ImageMinimizerPlugin.sharpMinify,
+						options: {
+							encodeOptions: {
+								encodeOptions: {
+									jpeg: {
+										// https://sharp.pixelplumbing.com/api-output#jpeg
+										quality: 100,
+									},
+									webp: {
+										// https://sharp.pixelplumbing.com/api-output#webp
+										lossless: true,
+									},
+									avif: {
+										// https://sharp.pixelplumbing.com/api-output#avif
+										lossless: true,
+									},
+
+									// png by default sets the quality to 100%, which is same as lossless
+									// https://sharp.pixelplumbing.com/api-output#png
+									png: {},
+
+									// gif does not support lossless compression at all
+									// https://sharp.pixelplumbing.com/api-output#gif
+									gif: {},
+								},
+							},
+						},
+					},
+					generator: [
+						{
+							// You can apply generator using `?as=webp`, you can use any name and provide more options
+							preset: 'webp',
+							implementation: ImageMinimizerPlugin.imageminGenerate,
+							options: {
+								plugins: ['imagemin-webp'],
+							},
+						},
+					],
+				}),
+			],
+		},
+	}
 }
